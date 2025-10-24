e5285a30bfc47f9838a5a04602fe41f55a9b553d
jhuequeman <huequeman@gmail.com>
2025-10-24 00:27:11 +0000

    carta aleatoria terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import "bootstrap";
 import "./style.css";
 
+let pinta = ['diamante', 'corazon', 'picas', 'trebol'];
+let numeroCarta = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Generador de cartas aleatorias
+window.onload = function generadorDeCartas() {
+  let numeroCartaAleatorio = numeroCarta[Math.floor(Math.random() * numeroCarta.length)];
+  let pintaAleatoria = pinta[Math.floor(Math.random() * pinta.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector('.top_left').classList.add(`${pintaAleatoria}`);
+  document.querySelector('.valor').innerHTML = numeroCartaAleatorio;
+  document.querySelector('.buttom_right').classList.add(`${pintaAleatoria}`);
 };
